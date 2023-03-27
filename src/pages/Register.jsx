922243193591e6dd8b8dcb0ca2f0a902c7d02eff
Mian Khizr Shah <khizrkshah@gmail.com>
2023-03-27 17:18:22 +0200

    Added the Register page functionality to allow the user to register an account into the database.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,122 @@
+import { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import "../css/homepagestyle.css";
+
 export function Register() {
-  return <div></div>;
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [address, setAddress] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  function registerUser(data) {
+    fetch("http://localhost:8081/api/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        navigate("/");
+      });
+  }
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleAddressChange = (event) => {
+    setAddress(event.target.value);
+  };
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = { email, password, address, firstName, lastName };
+    registerUser(data);
+  };
+
+  return (
+    <div className="App">
+      <div className="register">
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <b>Email</b>
+          </label>
+          <input
+            type="text"
+            id="Email"
+            placeholder="Enter Email"
+            onChange={handleEmailChange}
+            value={email}
+          />
+
+          <label>
+            <b>Password</b>
+          </label>
+          <input
+            type="password"
+            id="pword"
+            placeholder="Enter Password"
+            onChange={handlePasswordChange}
+            value={password}
+          />
+
+          <label>
+            <b>Address</b>
+          </label>
+          <input
+            type="text"
+            id="address"
+            placeholder="Enter Address"
+            onChange={handleAddressChange}
+            value={address}
+          />
+
+          <label>
+            <b>First Name</b>
+          </label>
+          <input
+            type="text"
+            id="firstName"
+            placeholder="Enter First Name"
+            onChange={handleFirstNameChange}
+            value={firstName}
+          />
+
+          <label>
+            <b>Last Name</b>
+          </label>
+          <input
+            type="text"
+            id="lastName"
+            placeholder="Enter Last Name"
+            onChange={handleLastNameChange}
+            value={lastName}
+          />
+
+          <button type="submit">Register</button>
+        </form>
+      </div>
+      <hr />
+      <footer className="footer">
+        <div className="constraint">Mian Khizr Shah &copy; 2023</div>
+      </footer>
+    </div>
+  );
 }
